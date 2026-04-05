acaf2ae78dad35be7f1902a2dbf3320339a86238
kanishk <voidaquacore@gmail.com>
2026-04-05 17:08:36 +0545

    Added histoy and refined CLI interface

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def operation(operand, num1, num2):
         return num1 - num2
     elif operand == "/":
         if num2 == 0:
-            return "cannot divide by zero"
+            print("cannot divide by zero")
         return num1 / num2
     elif operand == "*":
         return num1 * num2
@@ -23,34 +23,54 @@ def get_number(msg):
 
 def get_operand():
     while True:
-       operand = input("which operation? (+,-,*,/): ")
+       operand = input("choose operation (+,-,*,/): ")
        if operand in ["+","-","/","*"]:
            break
        else:
            print("invaild operator (-,+,/,* only)")
     return operand
 
+history = []
 
 while True:
+    choice = input(" choose: [h] -> history | [e] -> exit | [c] -> to calculate: ")
     
-    num1 = get_number("Enter first number: ")
-    num2 = get_number("Enter second number: ")
-    
-    operand = get_operand()
-
-    result = operation(operand, num1, num2)    
-            
-
-    if isinstance(result, int) or isinstance(result, float):
-        print(f"{num1} {operand} {num2} = {result}")
-    else:
-        print(result)
-    
-    exit = input("Do you want to exit? (y/n) : ")
-    if exit.lower() == "y":
+    if choice.lower() == "e":
         print("sayonara")
         break
     
+    elif choice.lower() == "c":
+
+        num1 = get_number("Enter first number: ")
+        num2 = get_number("Enter second number: ")
+        
+        operand = get_operand()
+
+        result = operation(operand, num1, num2)    
+                
+        history.append((num1,operand, num2, result))
+
+        if isinstance(result, int) or isinstance(result, float):
+            print("")
+            print(f"{num1} {operand} {num2} = {result}")
+            print("")
+        else:
+            print(result)
+    
+    elif choice.lower() == "h":
+        i = 1
+        if not history:
+            print("No calculations performed yet.")
+        else:
+            print("History ->")
+            for calc in history:
+                a, op, b , result = calc
+                print(f"{i}. {a} {op} {b} = {result}")
+                i += 1
+            print('')
+    else:
+        print("invalid input, Try again!")
+ 
         
 
 
